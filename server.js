e6932ea2546ab0dd41bf3faf17c19e12db502edd
gaston <gaston_a96@hotmail.com>
2022-02-03 00:43:36 -0300

    adding products

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+const Contenedor = require('./clase.js')
+const newClass = new Contenedor('products.json');
 
 const server = app.listen(PORT, () =>{
     console.log(`Escuchando en el puerto ${PORT}`);
@@ -13,3 +15,13 @@ server.on("Error", error => console.log(`Error en el servidor ${error}`));
 app.get('/', (req, res) =>{
     res.send('Bienvenido al servidor de Ash');
 })
+
+app.get('/products', (req, res) =>{
+    res.send( JSON.stringify(newClass.getAll() ));
+    console.log(newClass.getAll())
+})
+
+app.get('/randomProduct', (req, res) =>{
+    res.send([1,2,3,4,5]);
+})
+
